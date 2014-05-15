286ca6ce9022be5c648c85d9210ffc4ccb0e63af
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-05-15 15:21:49 -0700

    wrote index and new methods in Adventure Controller

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,23 @@
 class AdventuresController < ApplicationController
+  
+  def index
+    @adventures = Adventure.all
+
+    respond_to do |f|
+      f.html
+      f.json { render json: {adventures: @adventures.as_json (
+                            execpt: [:id, :library_id],
+                            include: {:pages => {except: :id}})}}
+  end
+
+  def new
+    @adventure = Adventure.new
+  end
+
+  def create
+
+  end
+
+
+
 end
