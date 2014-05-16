83f38bbbc44b4d97ad94f584abdeea58cbf205d2
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-05-15 17:54:40 -0700

    added show pages

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -3,11 +3,11 @@ class AdventuresController < ApplicationController
   def index
     @adventures = Adventure.all
 
-    respond_to do |f|
-      f.html
-      f.json { render json: {adventures: @adventures.as_json (
-                            execpt: [:id, :library_id],
-                            include: {:pages => {except: :id}})}}
+    # respond_to do |f|
+    #   f.html
+      # f.json { render json: {adventures: @adventures.as_json (
+                            # execpt: [:id, :library_id],
+                            # include: {:pages => {except: :id}})}}
   end
 
   def new
@@ -15,9 +15,21 @@ class AdventuresController < ApplicationController
   end
 
   def create
-
+    @adventure = Adventure.new (adventure_params)
+    @adventure.guid = SecureRandom.urlsafe_base64(10)
+    # binding.pry
+    # @adventurs.library_id = nil
+    @adventure.save
+    redirect_to root_path
   end
 
+  def show
+    @adventure = Adventure.find(params[:id])
+  end
 
+  private
 
+    def adventure_params
+      params.require(:adventure).permit(:title, :author, :guid, :library_id)
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,24 @@
 class PagesController < ApplicationController
+  #require 'PagesHelper'
+  before_action :load_adventure
+  before_action :load_page
+
+
+  def show
+  end
+
+
+
+
+  private 
+
+    def load_adventure
+      @adventure = Adventure.find params[:adventure_id]
+      redirect_to root_path if @adventure.blank?
+    end
+
+    def load_page
+      @page = @adventure.pages.find params[:id]
+      redirect_to root_path if @page.blank?
+    end
 end
